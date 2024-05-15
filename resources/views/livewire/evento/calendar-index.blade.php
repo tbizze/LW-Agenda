8e04053411d58221edbfdcd6feddf19cf78c228d
Tiago Bizze <tbizze@hotmail.com>
2024-05-15 14:08:50 -0300

    alterado configs do calendário e local dos scripts

--- resources/views/livewire/evento/calendar-index.blade.php
+++ resources/views/livewire/evento/calendar-index.blade.php
@@ -1,19 +1,32 @@
 <div>
-    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.9/index.global.min.js'></script>
+    
     <div wire:ignore id='calendar'></div>
 
-    <script>
-        document.addEventListener('livewire:initialized', function() {
-            var calendarEl = document.getElementById('calendar');
-            var calendar = new FullCalendar.Calendar(calendarEl, {
-                initialView: 'dayGridMonth',
-                timeZone: 'America/Sao_Paulo',
-                locale: "pt-br",
-                editable: true,
-                selectable: true,
-                events: @json($events),
+    {{-- Estes scripts serão inseridos nesta cláusula do layout. --}}
+    @push('scripts')
+        {{-- <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.9/index.global.min.js'></script> --}}
+        {{-- <script src='{{asset('imgs/index.global.min.js')}}'></script>
+        <script src='{{asset('imgs/daygrid/index.global.min.js')}}'></script> --}}
+        <script src='{{asset('storage/fullcalendar/core/index.global.min.js')}}'></script>
+        <script src='{{asset('storage/fullcalendar/daygrid/index.global.min.js')}}'></script>
+
+        <script>
+            document.addEventListener('livewire:initialized', function() {
+                var calendarEl = document.getElementById('calendar');
+                var calendar = new FullCalendar.Calendar(calendarEl, {
+                    buttonText: {
+                        today: 'Hoje',
+                    },
+                    initialView: 'dayGridMonth',
+                    timeZone: 'America/Sao_Paulo',
+                    locale: "pt-br",
+                    //height: 800,
+                    editable: true,
+                    selectable: true,
+                    events: @json($events),
+                });
+                calendar.render();
             });
-            calendar.render();
-        });
-    </script>
+        </script>
+    @endpush
 </div>
