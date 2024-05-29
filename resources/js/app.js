1b8043c0e2f02c66bedaf19e4476bf122c7ba001
Tiago Bizze <Tiago Bizze>
2024-05-28 23:53:41 -0300

    install alpinejs/mask

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+
+import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
+
+import mask from '@alpinejs/mask';
+Alpine.plugin(mask);
+
+ 
+Livewire.start()
