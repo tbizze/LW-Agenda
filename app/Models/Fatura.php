e7287949bafe09c7135f5dca2d6bbc1f3b8bb78e
Tiago Bizze <Tiago Bizze>
2024-05-30 21:41:14 -0300

    update model fatura: accessors

--- app/Models/Fatura.php
+++ app/Models/Fatura.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -31,6 +32,19 @@ class Fatura extends Model
         'dt_venc' => 'date:d/m/Y',
         'dt_pgto' => 'date:d/m/Y',
     ];
+
+    protected function valorFatura(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => currency_get_db($value),
+        );
+    }
+    protected function valorPgto(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => currency_get_db($value),
+        );
+    }
 
     /**
      * RELACIONAMENTO: A Fatura 'tem muitos' (hasMany) FaturaItems.
